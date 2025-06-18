48a719621f8a5ab8f8182cc45c5d267d2c16453a
seong su kim <know.warehouse02@gmail.com>
2025-06-18 19:30:43 +0900

    stores -zustand

---
+++ stores/CartStore.js
@@ -0,0 +1,14 @@
+// stores/CartStore.js
+import { create } from 'zustand';
+
+export const useCartStore = create((set) => ({
+  items: [],
+  addItem: (item) =>
+    set((state) => ({
+      items: [...state.items, item],
+    })),
+  removeItem: (id) =>
+    set((state) => ({
+      items: state.items.filter((item) => item.id !== id),
+    })),
+}));
