01fa63a2bdcd7096ae1bcc00421d98b8741cbd25
seong su kim <know.warehouse02@gmail.com>
2025-06-18 17:05:18 +0900

    about 경로 수정

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import About from './page/About.jsx';
+import About from './chapter5/About.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
